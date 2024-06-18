bb92d792904a9e89ebd66b855db3e5a2139efcb7
LeandroLeite <leandroleite.ll25@proton.me>
2024-06-18 15:35:55 -0300

    feat: criacao das telas de evente, criação edicao e visualizacao

--- eventify/src/app/app.routes.ts
+++ eventify/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { SignInComponent } from './views/account/sign-in/sign-in.component';
 import { SignUpComponent } from './views/account/sign-up/sign-up.component';
 import { MainComponent } from './views/main/main.component';
 import { authenticationGuard } from './services/security/guard/authentication.guard';
+import { MyEventsComponent } from './views/events/my-events/my-events.component';
+import { CreateEventsComponent } from './views/events/create-events/create-events.component';
+import { EditEventsComponent } from './views/events/edit-events/edit-events.component';
 
 export const routes: Routes = [
     {
@@ -14,14 +17,27 @@ export const routes: Routes = [
         component: SignUpComponent
     },
     {
-        path:'',
+        path: '',
         component: MainComponent,
         canActivate: [authenticationGuard],
         children: [
             {
-                path:'',
+                path: '',
                 component: MainComponent
-            }
+            },
+            {
+                path: 'my-events',
+                component: MyEventsComponent,
+            },
+            {
+                path: 'create-events',
+                component: CreateEventsComponent,
+            },
+            {
+                path: 'edit-events',
+                component: EditEventsComponent,
+            },
+
         ]
     }
 ];
--- eventify/src/app/views/main/main.component.ts
+++ eventify/src/app/views/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
-    
+    RouterModule,
+
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
