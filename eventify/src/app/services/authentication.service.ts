a24ba46470acbe566a8d1cf2700dd8e4a2caef4e
Carlos <carlos_eduardo_a.r@outlook.com>
2024-06-18 23:07:32 -0300

    feat: adicao do json-server e implementacao da coleta dos dados do formulario de login, bem o authentication.service

--- eventify/src/app/services/authentication.service.ts
+++ eventify/src/app/services/authentication.service.ts
@@ -1,9 +1,43 @@
+import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { UserCredential } from '../domain/dto/user-credential';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  async authenticate(credential: UserCredential) {
+    // console.log('trying to authenticate...');
+    // console.log(credential);
+
+    let apiResponse = await firstValueFrom(this.http.get<UserCredential[]>(`http://localhost:3000/user?email=${credential.email}&password=${credential.password}`));
+    console.log(apiResponse);
+    if (apiResponse == null || apiResponse.length != 1) {
+      throw new Error('dados invalidos');
+    }
+    return true;
+  }
+
+  logout() {
+    localStorage.clear();
+  }
+
+  isAuthenticated(): boolean {
+    let token = localStorage.getItem('token');
+
+    if (token != null) {
+      return true;
+    }
+    return false;
+  }
+
+  addCredentialsToLocalStorage(email: string) {
+    localStorage.setItem('email', email);
+    localStorage.setItem('token', new Date().toLocaleTimeString());
+  }
 }
--- eventify/src/app/views/account/sign-in/sign-in.component.ts
+++ eventify/src/app/views/account/sign-in/sign-in.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { Router } from '@angular/router';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { UserCredential } from '../../../domain/dto/user-credential';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  constructor(private router: Router, private authenticationService: AuthenticationService) {}
 
+  email = new FormControl(null);
+  password = new FormControl(null, [Validators.minLength(1), Validators.maxLength(10)])
+
+  ngOnInit(): void {
+    if( this.authenticationService.isAuthenticated()){
+      this.router.navigate(['account/sign-in']);
+    }
+  }
+
+  async login(){
+    let credential: UserCredential = {
+      email: this.email.value!,
+      password: this.password.value!
+    };
+    console.log(credential);
+  }
 }
