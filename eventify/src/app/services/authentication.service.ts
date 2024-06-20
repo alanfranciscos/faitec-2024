0e1d59ede77ec2539670c85830830aa47e5f1676
Carlos <carlos_eduardo_a.r@outlook.com>
2024-06-20 13:39:21 -0300

    feat: implementação da validação de credenciais do login

--- eventify/src/app/services/authentication.service.ts
+++ eventify/src/app/services/authentication.service.ts
@@ -11,17 +11,42 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient) { }
 
-  async authenticate(credential: UserCredential) {
-    // console.log('trying to authenticate...');
-    // console.log(credential);
+  // async authenticate(credential: UserCredential) {
+  //   // console.log('trying to authenticate...');
+  //   // console.log(credential);
 
-    let apiResponse = await firstValueFrom(this.http.get<UserCredential[]>(`http://localhost:3000/user?email=${credential.email}&password=${credential.password}`));
-    console.log(apiResponse);
-    if (apiResponse == null || apiResponse.length != 1) {
-      throw new Error('dados invalidos');
+  //   let apiResponse = await firstValueFrom(this.http.get<UserCredential[]>(`http://localhost:3000/user?email=${credential.email}&password=${credential.password}`));
+  //   console.log(apiResponse);
+  //   if (apiResponse == null || apiResponse.length != 1) {
+  //     throw new Error('dados invalidos');
+  //   }
+  //   return true;
+  // }
+
+
+  async authenticate(credential: UserCredential) {
+    try {
+      const apiResponse = await firstValueFrom(this.http.get<UserCredential[]>(`http://localhost:3000/user?email=${credential.email}`));
+  
+      if (apiResponse && apiResponse.length === 1) {
+        const user = apiResponse[0];
+        // Compare a senha fornecida com a senha armazenada no banco de dados (usando bcrypt ou outra biblioteca).
+        if (user.password === credential.password) {
+          return true; // Autenticação bem-sucedida
+        } else {
+          throw new Error('Senha incorreta');
+        }
+      } else {
+        throw new Error('Usuário não encontrado');
+      }
+    } catch (error) {
+      console.error('Erro ao autenticar:', error);
+      throw new Error('Erro ao autenticar');
     }
-    return true;
   }
+  
+
+
 
   logout() {
     localStorage.clear();
--- eventify/src/app/views/account/sign-in/sign-in.component.ts
+++ eventify/src/app/views/account/sign-in/sign-in.component.ts
@@ -3,22 +3,25 @@ import { AuthenticationService } from '../../../services/authentication.service'
 import { Router } from '@angular/router';
 import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { UserCredential } from '../../../domain/dto/user-credential';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent implements OnInit {
-  constructor(private router: Router, private authenticationService: AuthenticationService) {}
+  constructor(private router: Router, private authenticationService: AuthenticationService, private toastrService: ToastrService) {}
 
   email = new FormControl(null);
   password = new FormControl(null, [Validators.minLength(1), Validators.maxLength(10)])
+
+  isLoginIncorrect = false;
 
   ngOnInit(): void {
     if( this.authenticationService.isAuthenticated()){
@@ -31,6 +34,22 @@ export class SignInComponent implements OnInit {
       email: this.email.value!,
       password: this.password.value!
     };
-    console.log(credential);
+    try {
+      await this.authenticationService.authenticate(credential);
+      this.authenticationService.addCredentialsToLocalStorage(credential.email);
+      await this.router.navigate(['/']);
+      this.isLoginIncorrect = false;
+    } catch (e: any) {
+      // console.error(`erro: ${e}`);
+      // this.toastrService.error(e.message);
+      this.email.setValue(null);
+      this.password.setValue(null);
+      this.isLoginIncorrect = true;
+    }
+  }
+
+  isFormInvalid(){
+    let isValid = this.email.valid && this.password.valid;
+    return isValid ? false : true;
   }
 }
