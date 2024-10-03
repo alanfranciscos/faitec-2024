7bde34194d7bafcc97e690558ec889f4dc55c09a
Alan Silva <alanfranciscos@gmail.com>
2024-10-03 20:10:12 -0300

    feat? add maker on map

--- eventify/src/app/components/map/map.component.ts
+++ eventify/src/app/components/map/map.component.ts
@@ -5,7 +5,7 @@ import {
   OnChanges,
   SimpleChanges,
 } from '@angular/core';
-import { Map, TileLayer, MapOptions } from 'leaflet';
+import { Map, TileLayer, MapOptions, Marker } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 @Component({
@@ -24,7 +24,6 @@ export class MapComponent implements OnInit, OnChanges {
   private optionsMap: MapOptions = {
     maxBoundsViscosity: 1.0,
     minZoom: 2,
-    maxZoom: 10,
     center: [-14.239209931938646, -50.261992558398134],
     zoom: 4,
     layers: [
@@ -34,8 +33,22 @@ export class MapComponent implements OnInit, OnChanges {
     ],
   };
 
+  private addMarker(lat: number, lng: number): void {
+    if (this.map) {
+      const marker = new Marker([lat, lng], { draggable: true });
+      marker.addTo(this.map).bindPopup('Arraste-me!').openPopup();
+
+      marker.on('dragend', (event) => {
+        const target = event.target;
+        const position = target.getLatLng();
+        console.log('Local:', position.lat, position.lng);
+      });
+    }
+  }
+
   ngOnInit(): void {
     this.initMap();
+    this.addMarker(-14.239209931938646, -50.261992558398134);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -46,7 +59,7 @@ export class MapComponent implements OnInit, OnChanges {
 
   private initMap(): void {
     this.map = new Map(this.mapContainerId, this.optionsMap);
-    this.updateMapSize(); // Inicializa o tamanho do mapa com as propriedades iniciais
+    this.updateMapSize();
   }
 
   private updateMapSize(): void {
@@ -55,7 +68,7 @@ export class MapComponent implements OnInit, OnChanges {
       if (mapContainer) {
         mapContainer.style.width = this.width + 'px';
         mapContainer.style.height = this.height + 'px';
-        this.map.invalidateSize(); // Invalida o tamanho para ajustar a nova dimensão
+        this.map.invalidateSize();
       }
     }
   }
