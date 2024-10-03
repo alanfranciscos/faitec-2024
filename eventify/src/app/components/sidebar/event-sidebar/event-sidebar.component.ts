8c4ecb39995c30a1d9e612bca7b6847c6f07126a
Leandro Leite <leandroleite.ll25@proton.me>
2024-10-03 20:02:57 -0300

    feat: ajusted routes

--- eventify/src/app/components/sidebar/event-sidebar/event-sidebar.component.ts
+++ eventify/src/app/components/sidebar/event-sidebar/event-sidebar.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ItemComponent } from '../item/item.component';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { SideBarItensType } from './types';
+
 @Component({
   selector: 'app-event-layout',
   standalone: true,
@@ -11,35 +12,40 @@ import { SideBarItensType } from './types';
   styleUrl: './event-sidebar.component.scss',
 })
 export class EventLayoutComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
   @Input() eventName: string = 'Event Name';
   @Input() location: string = 'Local';
   @Input() date: string = '10/10/2010';
   @Input() creatorUser: string = 'John Doe';
   @Input() description: string = 'Palestras sobre inovações tecnológicas.';
   isCollapsed = false;
-  sidebarItens: Array<SideBarItensType> = [
-    {
-      title: 'General view',
-      routerLink: '/manage-event/general-view',
-      isSelected: false,
-      icon: 'far fa-map',
-    },
-    {
-      title: 'Expenses',
-      isSelected: false,
-      routerLink: '/manage-event/expenses',
-      icon: 'fas fa-dollar-sign',
-    },
-    {
-      title: 'Members',
-      isSelected: false,
-      routerLink: '/manage-event/members',
-      icon: 'fas fa-user-friends',
-    },
-  ];
+
+  sidebarItens: Array<SideBarItensType> = [];
 
   ngOnInit(): void {
+    const eventId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.sidebarItens = [
+      {
+        title: 'General view',
+        routerLink: '/event/' + eventId,
+        isSelected: false,
+        icon: 'far fa-map',
+      },
+      {
+        title: 'Expenses',
+        isSelected: false,
+        routerLink: `/event/${eventId}/expenses`,
+        icon: 'fas fa-dollar-sign',
+      },
+      {
+        title: 'Members',
+        isSelected: false,
+        routerLink: `/event/${eventId}/members`,
+        icon: 'fas fa-user-friends',
+      },
+    ];
+
     const activeRoute = this.router.url;
     this.sidebarItens.forEach((item) => {
       item.isSelected = item.routerLink === activeRoute;
