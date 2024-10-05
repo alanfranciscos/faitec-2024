c97a8f7c66da26e04b06dd947e64e7df738ef118
Leandro Leite <leandroleite.ll25@proton.me>
2024-10-05 00:30:24 -0300

    feat: expenses with backend-data

--- eventify/src/app/pages/events/manage-event/expenses/expenses.component.ts
+++ eventify/src/app/pages/events/manage-event/expenses/expenses.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { PaymentApproach } from '../../../../domain/model/event/paymentApproach.model';
 import { EventService } from '../../../../services/event/event.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { ExpansesResponse } from '../../../../domain/model/event/expense.model';
 
 interface Expense {
@@ -25,87 +25,20 @@ export class ExpensesComponent implements OnInit {
   totalExpenses!: TotalExpenses;
   eventExpanses!: ExpansesResponse;
   constructor(
+    private router: Router,
     private eventService: EventService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute // private route: Route
   ) {}
-  expenses = [
-    {
-      name: 'Example Name 1',
-      date: '01/01/2024',
-      description: 'Example Description 1',
-      amount: 100.0,
-    },
-    {
-      name: 'Example Name 2',
-      date: '02/01/2024',
-      description: 'Example Description 2',
-      amount: 200.0,
-    },
-    {
-      name: 'Example Name 3',
-      date: '03/01/2024',
-      description: 'Example Description 3',
-      amount: 300.0,
-    },
-    {
-      name: 'Example Name 4',
-      date: '04/01/2024',
-      description: 'Example Description 4',
-      amount: 400.0,
-    },
-    {
-      name: 'Example Name 5',
-      date: '05/01/2024',
-      description: 'Example Description 5',
-      amount: 500.0,
-    },
-    {
-      name: 'Example Name 6',
-      date: '06/01/2024',
-      description: 'Example Description 6',
-      amount: 600.0,
-    },
-    {
-      name: 'Example Name 7',
-      date: '07/01/2024',
-      description: 'Example Description 7',
-      amount: 700.0,
-    },
-    {
-      name: 'Example Name 8',
-      date: '08/01/2024',
-      description: 'Example Description 8',
-      amount: 800.0,
-    },
-    {
-      name: 'Example Name 9',
-      date: '09/01/2024',
-      description: 'Example Description 9',
-      amount: 900.0,
-    },
-    {
-      name: 'Example Name 10',
-      date: '10/01/2024',
-      description: 'Example Description 10',
-      amount: 1000.0,
-    },
-    {
-      name: 'Example Name 11',
-      date: '11/01/2024',
-      description: 'Example Description 11',
-      amount: 1100.0,
-    },
-  ];
 
-  paginatedExpenses: Expense[] = [];
   currentPage = 1;
   pageSize = 10;
   totalPages = 0;
 
   async ngOnInit() {
-    // arrumar isso daqui pq n ta pegando a rota n sei pq, eu setei como 1
-    let eventId = this.activatedRoute.snapshot.paramMap.get('id');
-    eventId = eventId == null ? '1' : eventId;
+    const url = this.router.url;
+    let eventId = url.split('/')[2];
+    eventId = eventId == null ? '-1' : eventId;
+
     console.log('eventid do expenses', eventId);
     const totalExpenses = await this.eventService.getTotalExpenses(eventId);
     this.totalExpenses = totalExpenses;
@@ -113,28 +46,35 @@ export class ExpensesComponent implements OnInit {
     this.paymentApproach = paymentApproach;
     const eventExpanses = await this.eventService.getEventExpanses(eventId);
     this.eventExpanses = eventExpanses;
+    this.eventExpanses = {
+      ...eventExpanses,
+      expanses: eventExpanses.expanses.map((expense) => ({
+        ...expense,
+        date: new Date(expense.date).toLocaleDateString(),
+      })),
+    };
 
-    this.totalPages = Math.ceil(this.expenses.length / this.pageSize);
+    this.totalPages = 0;
     this.updatePagination();
   }
 
   updatePagination() {
-    const startIndex = (this.currentPage - 1) * this.pageSize;
-    const endIndex = startIndex + this.pageSize;
-    this.paginatedExpenses = this.expenses.slice(startIndex, endIndex);
+    // const startIndex = (this.currentPage - 1) * this.pageSize;
+    // const endIndex = startIndex + this.pageSize;
+    // this.paginatedExpenses = this.expenses.slice(startIndex, endIndex);
   }
 
   previousPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.updatePagination();
-    }
+    // if (this.currentPage > 1) {
+    //   this.currentPage--;
+    //   this.updatePagination();
+    // }
   }
 
   nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-      this.updatePagination();
-    }
+    //   if (this.currentPage < this.totalPages) {
+    //     this.currentPage++;
+    //     this.updatePagination();
+    //   }
   }
 }
