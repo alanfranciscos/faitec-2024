c47b5ff9005ba9c09dbdae2ff5ca87b8f9f2e273
Leandro Leite <leandroleite.ll25@proton.me>
2024-10-07 22:24:49 -0300

    wip

--- eventify/src/app/components/header/header.component.ts
+++ eventify/src/app/components/header/header.component.ts
@@ -1,16 +1,32 @@
 import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { TooltipComponent } from '../tooltip/tooltip.component';
+import { DropdownlistComponent } from '../dropdownlist/dropdownlist.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, TooltipComponent],
+  imports: [RouterLink, TooltipComponent, DropdownlistComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
   @Input() ComponentName!: string | '#';
   @Input() textTooltip?: string;
-  @Input() tooltipPosition: 'top' | 'bottom' | 'left' | 'right' = 'top'; // Posição padrão
+  @Input() tooltipPosition: 'top' | 'bottom' | 'left' | 'right' = 'top';
+  isOpen = false;
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectOption(option: string) {
+    this.isOpen = false;
+  }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/account/login']); // Redireciona para a tela de login
+  }
 }
