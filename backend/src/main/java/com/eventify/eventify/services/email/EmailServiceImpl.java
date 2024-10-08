e1ce84ed3cabe7f98b21c1f1774d6fd74686d736
Alan Silva <alanfranciscos@gmail.com>
2024-10-08 19:38:07 -0300

    feat: sendgrid send email

--- backend/src/main/java/com/eventify/eventify/services/email/EmailServiceImpl.java
+++ backend/src/main/java/com/eventify/eventify/services/email/EmailServiceImpl.java
@@ -1,18 +1,27 @@
 package com.eventify.eventify.services.email;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.eventify.eventify.port.service.email.EmailService;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import com.eventify.eventify.port.service.email.EmailService;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class EmailServiceImpl implements EmailService {
+    private static final Logger logger = Logger
+            .getLogger(EmailServiceImpl.class.getName());
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${sendgrid.api.key}")
+    private String sendgridApiKey;
 
     @Value("${validation.code.expiration.minutes}")
     private String codeExpirationMinutes;
@@ -20,20 +29,39 @@ public class EmailServiceImpl implements EmailService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
-    private void sendEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromEmail);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
+    private void sendEmail(String to, String subject, String text) throws IOException {
+        Email from = new Email(this.fromEmail);
+        Email toEmail = new Email(to);
+        Content content = new Content("text/html", text);
+        Mail mail = new Mail(from, subject, toEmail, content);
 
-        mailSender.send(message);
+        SendGrid sg = new SendGrid(this.sendgridApiKey);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            if (response.getStatusCode() != 202) {
+                throw new IOException("Error sending email: " + response.getBody());
+            }
+
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+            throw e;
+        }
+
     }
 
-    public void sendConfirmationCode(String email, String code) {
+    @Override
+    public void sendConfirmationCode(String toEmail, String code) {
         String subject = "Eventify - Código de Verificação";
         String text = BodyEmail.creadeHtmlBodyToSendCode(code, codeExpirationMinutes);
-        this.sendEmail(email, subject, text);
+        try {
+            this.sendEmail(toEmail, subject, text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
