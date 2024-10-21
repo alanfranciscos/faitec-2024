5b4bc6ee62d53fbbca3aafcb577154db13a46cd7
Alan Silva <alanfranciscos@gmail.com>
2024-10-21 20:23:34 -0300

    feat: add default image in list evenets

--- eventify/src/app/components/card/card.component.ts
+++ eventify/src/app/components/card/card.component.ts
@@ -9,12 +9,18 @@ import { RouterLink } from '@angular/router';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   @Input() title: string = 'titulo';
   @Input() description: string = 'descripcion';
-  @Input() image: string = 'imagen';
+  @Input() image: string = '/assets/svg/logo.svg';
   @Input() startData: string = 'startData';
   @Input() endData: string = 'endData';
   @Input() status: string = 'color';
   @Input() url!: string;
+
+  ngOnInit(): void {
+    if (!this.image) {
+      this.image = '/assets/svg/logo.svg';
+    }
+  }
 }
